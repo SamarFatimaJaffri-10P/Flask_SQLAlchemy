65e4c3b0e13589ca7235e6860421d240e3f55201
SamarFatimaJaffri-10P <samar.fatima@10pearls.com>
2022-03-01 21:03:22 +0500

    Add attributes in models
    Add attributes (column) with size (for strings) and constraints

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///db.videos'
@@ -11,12 +11,24 @@ db = SQLAlchemy(app)
 
 # models will map to tables in the database
 class Customer(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(500), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    postcode = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
 
 
 class Order(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    order_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    ship_date = db.Column(db.DateTime)
+    deliver_date = db.Column(db.DateTime)
+    coupon_code = db.Column(db.String(50))
 
 
 class Product(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    price = db.Column(db.Integer, nullable=False)
