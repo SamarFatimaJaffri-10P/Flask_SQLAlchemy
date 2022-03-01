696692be30231469bafb1eaf0f7ac425baa337c8
SamarFatimaJaffri-10P <samar.fatima@10pearls.com>
2022-03-01 20:52:35 +0500

    Make model structure
    Create classes representing models which will map to tables in database i.e., each model will create a table in database

--- app.py
+++ app.py
@@ -7,3 +7,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///db.videos'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+
+# models will map to tables in the database
+class Customer(db.Model):
+    pass
+
+
+class Order(db.Model):
+    pass
+
+
+class Product(db.Model):
+    pass
